3ddb3a15848b0b51af2ab4beea2b3d7bb6de1987
Gabriel Barros <gabrielbarros@fainor.com.br>
2026-06-29 11:01:41 -0300

    chore: configure Astro site settings and add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://gabrielbarrosba.github.io',
+	base: '/mariacrm',
 	integrations: [
 		tailwind(),
 		starlight({
